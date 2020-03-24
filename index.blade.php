8dba3d090fd503275139811e50b2a580a1be2f66
satriyocipto <satriyociptokusumo123@gmail.com>
2020-03-24 18:21:18 +0700

    laravel brow

---
+++ index.blade.php
@@ -0,0 +1,99 @@
+<html>
+    <head>
+        <title>SCK_172</title>
+            </head>
+        <body bgcolor="cyan">
+            <style type="text/css">
+                {
+    margin: 0;
+    padding: 0;
+    font-family: century gothic;
+}
+header{
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+}
+ul{
+    float: right;
+    list-style-type: none;
+    margin-top: 25px;
+}
+ul li{
+    display: inline-block;
+}
+ul li a{
+    text-decoration: none;
+    color: #fff;
+    padding: 5px 20px;
+    border: 1px solid transparent;
+    transition: 0.6s ease;
+}
+ul li a:hover{
+    background-color: #fff;
+    color: #000;
+}
+ul li.active a{
+    background-color: #fff;
+    color: #000;
+}
+.logo img{
+    float: left;
+    width: 150px;
+    height: auto;
+}
+.main{
+    max-width: 1200px;
+    margin: auto;
+}
+.title{
+    position: absolute;
+    top: 50%;
+    left: 10%;
+    transfrom: translate(-50%,-50%):
+}
+.title h1{
+    color: white;
+    font-size: 70px;
+
+}
+.button{
+    position: absolute;
+    top: 62%;
+    left: 50%;
+    transfrom: translate(-50%,-50%):
+}
+.btn{
+    border: 1px solid #fff;
+    padding: 10px 30px;
+    columns: #fff;
+    text-decoration: none;
+    transition: 0.6s ease;
+}
+.btn:hover{
+    background-color: #fff;
+    color: #000;
+}
+</style>
+            <header>
+                <div class="main">
+                    <div class="logo">
+                    </div>
+                    <ul>
+                        <li class="active"><a href="/"><i>HOME</i></a></li>
+                        <li><a href="/services"><i>SERVICES</i></a></li>
+                        <li><a href="/gallery"><i>GALLERY</i></a></li>
+                        <li><a href="/abouts"><i>ABOUTS</i></a></li>
+                        <li><a href="/contacts"><i>CONTACTS</i></a></li>
+                    </ul>
+                </div>
+                <div class="title">
+                    <h1><b><i>AYU BELLA</i></b></h1>
+                </div>
+                <div class="button">
+                    <a href="/" class="btn"><b>WATCH VIDEO</b></a>
+                    <a href="/" class="btn"><b>LEARN MORE</b></a>
+                </div>
+            </header>
+        </body>
+</html>
